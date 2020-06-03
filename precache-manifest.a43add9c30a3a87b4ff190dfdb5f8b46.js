2e925635d7c9b9465c08b3ea1491897f09fa107b
Andry <gaxeyat298@chordmi.com>
2020-06-03 14:49:22 +0300

    Updates

---
+++ precache-manifest.a43add9c30a3a87b4ff190dfdb5f8b46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f510091a431d9588819aebf6d3b17f",
+    "url": "/devComNom/index.html"
+  },
+  {
+    "revision": "4198d98088da9ed8cc28",
+    "url": "/devComNom/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "ebecf64460ed2364467e",
+    "url": "/devComNom/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "4198d98088da9ed8cc28",
+    "url": "/devComNom/static/js/2.06515f63.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/devComNom/static/js/2.06515f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebecf64460ed2364467e",
+    "url": "/devComNom/static/js/main.b01c0e08.chunk.js"
+  },
+  {
+    "revision": "d8c7e3d53e3938bd564f",
+    "url": "/devComNom/static/js/runtime-main.f1867206.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/devComNom/static/media/persik.4e1ec840.png"
+  }
+]);
